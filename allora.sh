8130d3bbc1a514199f245802824665a2b76a0513
Weng Weiyu <weiyu.weng@u.nus.edu>
2024-07-04 17:26:18 +0800

    Update allora.sh

--- allora.sh
+++ allora.sh
@@ -46,56 +46,12 @@ echo -e "${BOLD}${DARK_YELLOW}Installing python3...${RESET}"
 execute_with_prompt "sudo apt install python3 python3-pip -y"
 echo
 
-echo -e "${BOLD}${DARK_YELLOW}Installing Docker...${RESET}"
-execute_with_prompt 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg'
-echo
-execute_with_prompt 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
-echo
-execute_with_prompt 'sudo apt-get update'
-echo
-execute_with_prompt 'sudo apt-get install docker-ce docker-ce-cli containerd.io -y'
-echo
-
-echo -e "${BOLD}${DARK_YELLOW}Checking docker version...${RESET}"
-execute_with_prompt 'docker version'
-echo
-
-echo -e "${BOLD}${DARK_YELLOW}Installing Docker Compose...${RESET}"
-VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
-echo
-execute_with_prompt 'sudo curl -L "https://github.com/docker/compose/releases/download/'"$VER"'/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose'
-echo
-execute_with_prompt 'sudo chmod +x /usr/local/bin/docker-compose'
-echo
-
-echo -e "${BOLD}${DARK_YELLOW}Checking docker-compose version...${RESET}"
-execute_with_prompt 'docker-compose --version'
-echo
-
 if ! grep -q '^docker:' /etc/group; then
     execute_with_prompt 'sudo groupadd docker'
     echo
 fi
 
 execute_with_prompt 'sudo usermod -aG docker $USER'
-echo
-
-echo -e "${BOLD}${DARK_YELLOW}Installing Go...${RESET}"
-execute_with_prompt 'cd $HOME'
-echo
-execute_with_prompt 'ver="1.21.3" && wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"'
-echo
-execute_with_prompt 'sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"'
-echo
-execute_with_prompt 'rm "go$ver.linux-amd64.tar.gz"'
-echo
-execute_with_prompt 'echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile'
-echo
-execute_with_prompt 'source $HOME/.bash_profile'
-echo
-
-echo -e "${BOLD}${DARK_YELLOW}Checking go version...${RESET}"
-execute_with_prompt 'go version'
 echo
 
 echo -e "${BOLD}${UNDERLINE}${DARK_YELLOW}Installing Allorand...${RESET}"
